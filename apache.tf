cb7d1dcb8cfc5b43cc80031e46fe1f1ee4278402
Ahammad441 <syedaliahammad1@gmail.com>
2022-09-03 12:23:55 +0530

    updated grafana ec2

--- apache.tf
+++ apache.tf
@@ -52,7 +52,7 @@ resource "aws_instance" "grafana" {
   ami           = "ami-0b89f7b3f054b957e"
   instance_type = "t2.micro"
   subnet_id = aws_subnet.private[0].id
-  vpc_security_group_ids = [aws_security_group.grafana.id]
+  vpc_security_group_ids = [aws_security_group.apache.id]
   tags = {
     Name = "Stage-grafana"
   }
